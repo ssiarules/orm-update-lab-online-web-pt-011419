46e53e7a0cdfc6312845e51abc22e6f49c6a2818
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-05 23:57:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -74,7 +74,7 @@ class Student
  end
  
  def update
-   sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
+   sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
    DB[:conn].execute(sql, self.name, self.grade, self.id)
 end 
 
