2b3581c63cac6395d821cb17ea90570ae2a59972
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-05 23:53:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,7 +41,7 @@ class Student
     
    DB[:conn].execute(sql, self.name, self.grade)
    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-   #binding.pry 
+   binding.pry 
     end 
    end 
    
